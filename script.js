dbcf08490d5a0ecd3316b55126d25a5ef727805c
KirMac <komarova1610@yandex.ru>
2023-03-04 11:36:11 +0300

    make start button logic and create window with inputs

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const boardContainer = document.querySelector('.game__board');
+const boardContainer = document.querySelector(".game__board");
+const startBtn = document.querySelector(".start");
+const home = document.querySelector(".home");
 
 const gameBoard = (function () {
   // Game array
@@ -17,22 +19,75 @@ const displayControl = (function () {
     // Render logic
 
     // Probably can do it better with reduce;
-    let boardHTML = '';
+    let boardHTML = "";
     let curElem = 0;
     for (row of curBoardArr) {
       for (cell of row) {
         boardHTML += `<div class="board__cell" data-coordinates="${curBoardArr.indexOf(
           row
         )};${curElem}" data-is-taken="false"></div>`;
-        console.log(`CUR ELEM`, curElem);
         curElem === 2 ? (curElem = 0) : curElem++;
       }
     }
 
-    boardContainer.insertAdjacentHTML('afterbegin', boardHTML);
+    boardContainer.insertAdjacentHTML("afterbegin", boardHTML);
   };
 
+  const renderNameInput = function () {};
+
   return { renderBoard };
+})();
+
+const gameControl = (function () {
+  // Control game flow;
+
+  // Start game;
+
+  function startGame() {
+    // Listen to start button
+
+    startBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      home.insertAdjacentHTML(
+        "afterbegin",
+        `
+			<div class="inputs">
+			<form action="input__form">
+				<fieldset class="inputs__fieldset">
+					<legend class="inputs__legend">Enter the names of players</legend>
+					<input
+						type="text"
+						class="input__player1-name inputs__elem"
+						data-player-name="one"
+					/>
+					<input
+						type="text"
+						class="input__player2-name inputs__elem"
+						data-player-name="two"
+					/>
+					<button class="inputs__button button" type="submit">Start playing</button>
+				</fieldset>
+			</form>
+		</div>
+			`
+      );
+    });
+  }
+
+  // Listen to submit names button
+
+  function submitNames() {
+    document.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (e.target.classList.contains("inputs__button")) {
+        const nameOne = document.querySelector(".input__player1-name").value;
+        const nameTwo = document.querySelector(".input__player2-name").value;
+        if (nameOne === "" || nameTwo === "") return;
+      }
+    });
+  }
+
+  return { startGame, submitNames };
 })();
 
 function createPlayer(name) {
@@ -43,7 +98,6 @@ function createPlayer(name) {
   };
 }
 
-console.log(displayControl);
 displayControl.renderBoard(gameBoard.board);
-console.log(gameBoard);
-console.log(gameBoard.board);
+gameControl.startGame();
+gameControl.submitNames();
